9a3395faff446130ffad2ebd12747c1f393782b1
DMZ-11 <mendez.dan@gmail.com>
2025-01-17 11:06:30 -0600

    Added save password to SQLite3 database

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from tkinter import *
 from tkinter import messagebox
 from random import randint, choice, shuffle
 import pyperclip
+import sqlite3
+
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 def generate_password():
-
     letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
                'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
                'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
@@ -24,6 +25,7 @@ def generate_password():
     password_entry.insert(0, password)
     pyperclip.copy(password)
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
     website = website_entry.get()
@@ -32,19 +34,43 @@ def save():
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Oops", message="Please make sure you haven't "
-                                                              "let any fields empty!")
+                                                  "let any fields empty!")
 
     else:
 
-        is_ok = messagebox.askokcancel(title= website, message= f"Details: \nEmail: {email} "
-                                                     f"\nPassword: {password}"
-                                                    f"\nSave?")
+        is_ok = messagebox.askokcancel(title=website, message=f"Details: \nEmail: {email} "
+                                                              f"\nPassword: {password}"
+                                                              f"\nSave?")
         if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"{website} | {email} | {password}\n")
-                website_entry.delete(0, last=END)
-                email_entry.delete(0, last=END)
-                password_entry.delete(0, last=END)
+            # Connect to a (new) database
+            conn = sqlite3.connect("password_manager.db")
+
+            # Create a cursor
+            cur = conn.cursor()
+
+            # Create a "password" table
+            cur.execute('''CREATE TABLE IF NOT EXISTS password_manager (website TEXT, email TEXT, password TEXT)''')
+
+            # Insert form data
+            password_insert_query = '''INSERT INTO password_manager (website, email, password) VALUES(?, ?, ?)'''
+
+            password_insert_tuple = (website, email, password)
+
+            cur.execute(password_insert_query, password_insert_tuple)
+
+            # Commit the changes
+            conn.commit()
+
+            # Close the connection
+            cur.close()
+            conn.close()
+
+            # Clear form
+            website_entry.delete(0, last=END)
+            # email_entry.delete(0, last=END)
+            password_entry.delete(0, last=END)
+
+            website_entry.focus()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -57,7 +83,7 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-#Labels
+# Labels
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 
@@ -67,21 +93,21 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-#Entries
+# Entries
 website_entry = Entry()
 website_entry.grid(row=1, column=1, columnspan=2, sticky="EW")
 website_entry.focus()
 
 email_entry = Entry()
-email_entry.grid(row= 2, column=1, columnspan=2, sticky="EW")
+email_entry.grid(row=2, column=1, columnspan=2, sticky="EW")
 email_entry.insert(0, "mymail@mail.com")
 
 password_entry = Entry()
-password_entry.grid(row= 3, column=1, sticky="EW")
+password_entry.grid(row=3, column=1, sticky="EW")
 
-#Buttons
+# Buttons
 generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row= 3, column=2, sticky="EW")
+generate_password_button.grid(row=3, column=2, sticky="EW")
 
 add_button = Button(text="Add", width=35, command=save)
 add_button.grid(row=4, column=1, columnspan=2, sticky="EW")
